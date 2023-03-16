7aad399a4e76cc4e123192bad30d881752cd29e2
marwa19-marwa <marwazerrouk19@gmail.com>
2023-03-16 19:44:44 +0100

    size c datatype

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
-	printf("size of char:%c byte(s)", sizeof(char));
+	printf("size of char:%d byte(s)", sizeof(char));
 	printf("size of int:%d byte(s)", sizeof(int));
 	printf("size of long int:%d byte(s)", sizeof(long int));
 	printf("size of long long int:%d byte(s)", sizeof(long long int));
