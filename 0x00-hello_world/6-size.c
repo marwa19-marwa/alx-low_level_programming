4604a4202ac90a4cc62223e3e604dc5297f789b7
marwa19-marwa <marwazerrouk19@gmail.com>
2023-03-16 19:42:03 +0100

    size of datatypes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("size of char:%c byte(s)", sizeof(char));
+	printf("size of int:%d byte(s)", sizeof(int));
+	printf("size of long int:%d byte(s)", sizeof(long int));
+	printf("size of long long int:%d byte(s)", sizeof(long long int));
+	printf("size of float:%d byte(s)", sizeof(float));
+	return (0);
+}
