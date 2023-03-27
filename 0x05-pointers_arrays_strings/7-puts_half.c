38106922551fa7e3e6c094a86059cee744a9bed6
marwa19-marwa <marwazerrouk19@gmail.com>
2023-03-27 23:12:36 +0000

    task7 pointers

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *puts_half - print half of a string
+ *should print the last n characters of the string
+ *where n = (length_of_the_string - 1) / 2
+ *@str: input
+ *Return: half of input
+**/
+void puts_half(char *str)
+{
+	int a, n, longi;
+
+	for (a = 0; str[a] != '\0'; a++)
+		longi++;
+	n = (longi / 2);
+
+	if ((longi % 2) == 1)
+		n = ((longi + 1) / 2);
+
+	for (a = n; str[a] != '\0'; a++)
+		_putchar(str[a]);
+	_putchar('\n');
+}
