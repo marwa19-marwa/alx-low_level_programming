8102cdba6684df556ed028dbb5fe09ef35af17c7
marwa19-marwa <marwazerrouk19@gmail.com>
2023-03-27 23:29:39 +0000

    task8 pointers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+include "main.h"
+/**
+ *print_array - print n numbers of an array
+ *@a: the name of the array
+ *@n: number of array elts to be printed
+ *Return: a et n inputs
+**/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+	{
+		printf("%d, ", a[i]);
+		if (i == (n - 1))
+		{
+			printf("%d", a[n - 1]);
+		}
+		printf("\n");
+	}
+}
