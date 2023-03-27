1d3d58d9d93ebf496c731081bec82598985ffd46
marwa19-marwa <marwazerrouk19@gmail.com>
2023-03-27 22:59:24 +0000

    task6 pointers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ *puts2 - function prints every other character of a string
+ *starting with the first character
+ *followed by a new line
+ *@str: input
+ *Return: print
+**/
+void puts2(char *str)
+{
+	int longi = 0;
+	int t = 0;
+
+	char *y = str;
+
+	int 0;
+
+	while (*y != '\0')
+	{
+		y++;
+		longi++;
+	}
+	t = longi - 1;
+
+	for (m = 0; m <= t; m++)
+	{
+		if (m % 2 == 0)
+	{
+			_putchar(str[m]);
+	}
+	}
+	_putchar('\n');
+}
