d44326ce52fd2bc85dba7a8d7fe6b13ae0af3be6
marwa19-marwa <marwazerrouk19@gmail.com>
2023-04-04 22:18:32 +0000

    task1 pointers

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_memcpy - copies memory area
+ *@dest: memory where its stored
+ *@src: memory where is copied
+ *@n: number of bytes
+ *
+ *Return: copied memory with n bytes changed
+**/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int r = 0;
+	int n = i;
+
+	for (; r < i; r++)
+	{
+		dest[r] = src[r];
+		n--;
+	}
+	return (dest);
+}
